4ed2fca715aead9a2c1694281c8af1641c5ce72f
Simon Zimmermann <simonz05@gmail.com>
2010-11-13 23:07:30 +0100

    add template inclusion tag

--- notice/templatetags/notice.py
+++ notice/templatetags/notice.py
@@ -1,15 +1,19 @@
 import os
-from django import template
 
-import notice
+from django import template
+from django.conf import settings
 
 register = template.Library()
+NOTICE_JS = getattr(settings, 'NOTICE_JS', None)
 
-NOTICE_JS = None
+@register.inclusion_tag('display_notice.html')
+def display_notice():
+    return {}
+
+@register.simple_tag
 def include_notice():
     global NOTICE_JS
-    if NOTICE_JS is None:
-        path = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+    if not NOTICE_JS:
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
         NOTICE_JS = open(os.path.join(path, 'media', 'js', 'jquery.notice.js')).read()
     return '''<script type="text/javascript">%s</script>''' % NOTICE_JS
-register.simple_tag(include_notice)
