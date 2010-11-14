efbe6920d4a9987ba1d12b0c4840167b8347563f
Simon Zimmermann <simonz05@gmail.com>
2010-11-14 15:36:18 +0100

    try to fix setup.py file

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from distutils.core import setup
 import os
+from setuptools import setup, find_packages
 
 setup(
-    name = 'django-notice',
-    packages = ['notice'],
-    version='0.1.0',
-    description='django-notice is an application to send messages.',
-    long_description=open('README').read(),
     author='Simon Zimmermann',
     author_email='simonz05@gmail.com',
-    url='http://github.com/simonz05/django-notice',
+    description='django-notice is an application to send messages.',
+    include_package_data=True,
+    install_requires=['django-piston', 'setuptools_git'],
     license='GPL',
-    install_requires=['django-piston'],
+    long_description=open('README').read(),
+    name='django-notice',
+    packages=find_packages(),
+    url='http://github.com/simonz05/django-notice',
+    version='0.1.1',
+    zip_safe=False,
     classifiers=[
-        'Development Status :: 1 - Beta',
+        'Development Status :: 2 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
